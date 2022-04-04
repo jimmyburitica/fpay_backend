59d6fee8e46f7c429c262a86beb50169377e873c
Jimmy Buriticá <jimmyburitica@hotmail.com>
2022-04-04 16:34:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+
+## Fpay Backend Challenge
+
+### By: Jimmy Buriticá
+
+For this challenge, I used the following software:
+
+- PHP v8.0.9
+- Laravel v9.6.0
+- MariaDB v10.4.20
+
+## How to Setup
+
+1. Clone GitHub repo for this project locally.
+2. Install Composer Dependencies.
+
+```
+composer install
+```
+
+3. Install NPM Dependencies.
+
+```
+npm install
+```
+
+4. Create .env file. Make a copy of the .env.example file and create a .env
+
+```
+cp .env.example .env
+```
+
+5. Generate an app encryption key.
+
+```
+php artisan key:generate
+```
+6. Create an empty database for the application.
+7. In the .env file, add database information to allow Laravel to connect to the database.
+
+```
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=laravel
+DB_USERNAME=root
+DB_PASSWORD=
+```
+
+8. Migrate the database. Once your credentials are in the .env file, now you can migrate your database.
+
+```
+php artisan migrate
+```
+
+9. Seed the database (Optional). Fills your database with starter data.
+
+```
+php artisan db:seed
+```
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::orderBy('name')->get();
+        return UserResource::collection($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'avatar' => 'required',
+        ]);
+        User::create($request->all());
+        return response()->json(['message' => 'success']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
